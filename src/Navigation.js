14079317232199df113dff37e1900b45658bb248
Mahaprabhu <37104647+mahaprabhu1994@users.noreply.github.com>
2022-06-07 14:30:06 +0530

    three

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,7 +4,7 @@ import './Navigation.css';
 function Navigation() {
    return (
       <div className="navbar">
-         <h1 className="logo">Meme Generator-one-1</h1>
+         <h1 className="logo">Meme Generator-two - 2</h1>
          <h5 className="menu">  React Course - Project 3</h5>
       </div>
    )
